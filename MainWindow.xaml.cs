60be2117d4f856c88bdb5f72d9f8fe4c55bae482
Chip Locke <wd@teleri.net>
2019-08-23 00:59:03 -0700

    a thing that doesn't work

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,31 @@ namespace test_uwp_netcore3
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TabView_ChildChanged(object sender, EventArgs e)
+        {
+            var host = (WindowsXamlHost)sender;
+            var tabView = (Microsoft.Toolkit.Uwp.UI.Controls.TabView)host.Child;
+            if (tabView == null)
+            {
+                return;
+            }
+
+            tabView.Header = new TextBlock { Text = "A dumb header", FontSize = 16 };
+            tabView.Items.Add(new TextBlock { Text = "A tab?" });
+            tabView.Items.Add(new TextBlock { Text = "Another tab?" });
+
+            //tabView.Resources.Add(new KeyValuePair<object, object>("TabViewItemHeaderMinHeight", (double)40));
+            //tabView.Resources.Add(new KeyValuePair<object, object>("TabViewItemHeaderMinWidth", (double)40));
+            //tabView.Resources.Add(new KeyValuePair<object, object>("TabViewItemHeaderMaxWidth", (double)200));
+            /*
+                  <controls:TabView.Resources>
+        <x:Double x:Key="TabViewItemHeaderMinHeight">40</x:Double>
+        <x:Double x:Key="TabViewItemHeaderMinWidth">48</x:Double>
+        <x:Double x:Key="TabViewItemHeaderMaxWidth">200</x:Double>
+      </controls:TabView.Resources>
+             */
         }
     }
 }
